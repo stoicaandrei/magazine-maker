5a7fb45d0ec38cb040eaa5916f89fb5affb25d4f
Andrei Stoica <andrei00stoica@gmail.com>
2020-12-24 01:48:56 +0200

    Add possibility of selecting a certain magazine to MagazineContext.tsx

--- src/contexts/MagazineContext.tsx
+++ src/contexts/MagazineContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
 import { projectFirestore } from 'firebase_config';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
@@ -9,19 +9,29 @@ type ContextProps = {
   fetching: boolean;
   magazines: Magazine[];
   error: any;
+  currentMagazine?: Magazine;
+  selectMagazine: (id: string) => void;
 };
 
 export const MagazineContext = createContext<ContextProps>({} as ContextProps);
 
 export const MagazineProvider: React.FC = ({ children }) => {
+  const [selectedId, setSelectedId] = useState('none');
+
   const magazinesRef = projectFirestore.collection('magazines');
   const magazinesQuery = magazinesRef.limit(10);
   const [magazines, fetching, error] = useCollectionData<Magazine>(magazinesQuery, {
     idField: 'id',
   });
 
+  const currentMagazine = magazines?.find((item) => item.id === selectedId);
+
+  const selectMagazine = (id: string) => setSelectedId(id);
+
   return (
-    <MagazineContext.Provider value={{ fetching, magazines: magazines || [], error }}>
+    <MagazineContext.Provider
+      value={{ fetching, magazines: magazines || [], error, currentMagazine, selectMagazine }}
+    >
       {children}
     </MagazineContext.Provider>
   );

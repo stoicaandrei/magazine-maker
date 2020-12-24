b3194c730b593bc8cc4a407e767fd43b21aac755
Andrei Stoica <andrei00stoica@gmail.com>
2020-12-24 23:07:45 +0200

    Fix PageSelector.tsx behavior

--- src/views/MagazineView/PageSelector.tsx
+++ src/views/MagazineView/PageSelector.tsx
@@ -1,21 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 
 import { MagazineContext } from 'contexts';
 
 const PageSelector: React.FC = () => {
+  const [inputPage, setInputPage] = useState<number | ''>();
   const { currentMagazine, currentPage, jumpToPage } = useContext(MagazineContext);
 
   const pageCount = currentMagazine?.pageUrls.length || 1;
   const lastPage = pageCount - 1;
+
+  useEffect(() => setInputPage(currentPage), [setInputPage, currentPage]);
 
   return (
     <div className="page-selector-container">
       <input
         className="page-input"
         type="text"
-        value={currentPage}
+        value={inputPage}
         onChange={(e) => {
-          const page = parseInt(e.target.value) || 0;
+          if (!e.target.value) return setInputPage('');
+
+          const page = parseInt(e.target.value) || undefined;
+          if (typeof page === 'undefined') return;
+
           jumpToPage(page);
         }}
       />

7808d3a6df179f6afc8d6ad887a273752f5ba016
Andrei Stoica <andrei00stoica@gmail.com>
2020-12-24 02:16:34 +0200

    Refactor code at GalleryItem.tsx

--- src/views/Gallery/GalleryItem.tsx
+++ src/views/Gallery/GalleryItem.tsx
@@ -7,13 +7,12 @@ type Props = {
 };
 
 const GalleryItem: React.FC<Props> = ({ item }) => {
+  const backgroundStyle = { background: `url(${item.thumbnailUrl})`, backgroundSize: 'cover' };
+
   return (
     <div className="magazine-container">
       <a href={`/magazine/${item.id}`} target="_blank" rel="noreferrer">
-        <div
-          className="cover"
-          style={{ background: `url(${item.thumbnailUrl})`, backgroundSize: 'cover' }}
-        />
+        <div className="cover" style={backgroundStyle} />
         <div className="body">
           <div className="title">{item.title}</div>
         </div>
